60be9db7a8f2e79fb6ddf0df7cce0ad4f59343df
codemaker-kim <tnals72441@daum.net>
2024-12-04 10:43:10 +0900

    11650번 해결
    메모리 너무 많이 씀
    속도도 개느림

--- src/main/java/baekjoon/b11650.java
+++ src/main/java/baekjoon/b11650.java
@@ -3,28 +3,60 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class b11650 {
+class Dot implements Comparable {
+    private int x;
+    private int y;
+
+    Dot(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Dot d = (Dot) o;
+
+        if(d.getX() > this.x)
+            return -1;
+        else if(d.getX() < this.x)
+            return 1;
+        else
+        {
+            return d.getY() > this.y ? -1 : 1;
+        }
+    }
+}
+
+public class b11650{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        Stack<String> st;
+        ArrayList<Dot> dots = new ArrayList<>();
 
         for(int i=0; i<T; i++) {
-            st = new Stack<>();
-            String[] s = br.readLine().split("");
-            for(String str : s) {
-                if(st.isEmpty())
-                    st.push(str);
-                else if(st.peek().equals(str) || st.peek().equals(")"))
-                    st.push(str);
-                else
-                    st.pop();
-            }
-            System.out.println(st.isEmpty() ? "YES" : "NO");
+            int[] s = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            dots.add(new Dot(s[0], s[1]));
         }
+
+        Collections.sort(dots);
+
+        dots.forEach((d) -> System.out.println(d.getX() + " " + d.getY()));
     }
 }
+
