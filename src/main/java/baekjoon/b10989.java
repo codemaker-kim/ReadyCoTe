ec18c832e4cc8ccb9bea465c4b7158842fae8607
codemaker-kim <tnals72441@daum.net>
2024-12-18 00:06:32 +0900

    10989번 해결
    BufferedWriter와 OutputStream 활용하여 해결

--- src/main/java/baekjoon/b10989.java
+++ src/main/java/baekjoon/b10989.java
@@ -1,13 +1,12 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class b10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         short[] arr = new short[N];
@@ -18,7 +17,9 @@ public class b10989 {
 
         Arrays.sort(arr);
         for(short i : arr) {
-            System.out.println(i);
+            bw.write(i+"\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
