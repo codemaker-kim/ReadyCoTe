9d85aceaef541b974fc72cbfa54ac7ba51386483
codemaker-kim <tnals72441@daum.net>
2024-12-18 00:00:47 +0900

    10989번 실패
    원시 배열 형태로 바꾸어서 진행하였는데도 시간 초과

--- src/main/java/baekjoon/b10989.java
+++ src/main/java/baekjoon/b10989.java
@@ -3,21 +3,20 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class b10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        ArrayList<Short> arr = new ArrayList<>();
+        short[] arr = new short[N];
 
         for(int i=0; i<N; i++) {
-            arr.add(Short.parseShort(br.readLine()));
+            arr[i] = Short.parseShort(br.readLine());
         }
 
-        Collections.sort(arr);
+        Arrays.sort(arr);
         for(short i : arr) {
             System.out.println(i);
         }
