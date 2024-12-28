cdc1cdee95533693c5eaedcba55758b17f1349b1
codemaker-kim <tnals72441@daum.net>
2024-12-28 22:05:19 +0900

    18110번 해결
    역시 속도는 BufferedReader ㅋㅋ

--- src/main/java/baekjoon/b18110.java
+++ src/main/java/baekjoon/b18110.java
@@ -1,17 +1,20 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class b18110 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 의견 없으면 -> 난이도 0
         // 의견이 하나 이상 있다면, 30% 절사평균으로 결정
         // 35퍼센트 시간초과
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         if(n==0){
             System.out.println(0);
             return;
@@ -20,7 +23,7 @@ public class b18110 {
         int[] opinions = new int[n];
 
         for(int i=0; i<n; i++) {
-            opinions[i] = sc.nextInt();
+            opinions[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.parallelSort(opinions);
