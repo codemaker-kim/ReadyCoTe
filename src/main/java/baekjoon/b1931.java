5a91d5b75899415c71a6c32975744e2b7906c639
codemaker-kim <tnals72441@daum.net>
2025-11-12 18:09:59 +0900

    1931번 해결!

--- src/main/java/baekjoon/b1931.java
+++ src/main/java/baekjoon/b1931.java
@@ -29,25 +29,19 @@ public class b1931 {
             meetingTimes.add(meeting);
         }
 
-        // 회의 종료 시간을 기준으로 정렬,
-        // 회의 종료 시간이 같다면, 회의 시작 시간 기준으로 정렬
+        // 회의 시간이 가장 짧고, 시작시간이 가장 작은 회의
         meetingTimes.sort(
                 comparingInt((Integer[] meeting) -> meeting[1])
                         .thenComparingInt(meeting -> meeting[0])
+                        .thenComparingInt(meeting -> meeting[1] - meeting[0])
         );
 
         Integer[] beforeMeeting = meetingTimes.get(0);
 
-        for (int i = 0; i < meetingTimes.size(); i++) {
-            // 회의 확인 처리
+        for (int i = 1; i < meetingTimes.size(); i++) {
             Integer[] nextMeeting = meetingTimes.get(i);
 
-            // 이전 회의 종료 시간
-            int beforeMeetingFinishTime = beforeMeeting[1];
-
-            // 이전 회의 종료 시간이 다음 회의 시작 시간보다 작거나 같으면
-            // 갯수를 하나 증가시키고, 이전 회의를 다음 회의로 변경
-            if (beforeMeetingFinishTime <= nextMeeting[0]) {
+            if (beforeMeeting[1] <= nextMeeting[0]) {
                 result++;
                 beforeMeeting = nextMeeting;
             }
