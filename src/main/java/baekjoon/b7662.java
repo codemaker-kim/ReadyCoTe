a34c800505b325e7f1d285dc8f2d072b3fc0964a
codemaker-kim <tnals72441@daum.net>
2025-11-16 02:46:56 +0900

    우선순위 큐 정합성 로직 작성 중

--- src/main/java/baekjoon/b7662.java
+++ src/main/java/baekjoon/b7662.java
@@ -3,7 +3,11 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class b7662 {
@@ -28,6 +32,7 @@ public class b7662 {
             int k = Integer.parseInt(br.readLine());
             PriorityQueue<Integer> max_pq = new PriorityQueue<>(Collections.reverseOrder());
             PriorityQueue<Integer> min_pq = new PriorityQueue<>();
+            Map<Integer, Integer> checker = new HashMap<>();
 
             for (int j = 0; j < k; j++) {
                 String[] input = br.readLine().split(" ");
@@ -39,25 +44,60 @@ public class b7662 {
                     case INSERT:
                         max_pq.offer(value);
                         min_pq.offer(value);
+                        checker.put(value, checker.getOrDefault(value, 0) + 1);
                         break;
                     case DELETE:
-                        deleteValue(max_pq, min_pq, value);
+                        Integer deletedValue = deleteValue(max_pq, min_pq, value);
+                        Integer put = checker.put(deletedValue, checker.getOrDefault(deletedValue, 1) - 1);
+
+                        if (put.equals(0)) {
+                            checker.remove(deletedValue);
+                        }
+
                         break;
                 }
             }
 
+            List<Integer> result = new ArrayList<>();
 
+            while (!max_pq.isEmpty() && !min_pq.isEmpty()) {
+                Integer maxValue = max_pq.peek();
+                Integer minValue = min_pq.peek();
+
+                if (checker.containsKey(maxValue) && checker.get(maxValue) > 0) {
+                    result.add(maxValue);
+                    max_pq.poll();
+                } else {
+                    max_pq.poll();
+                }
+
+                if (checker.containsKey(minValue) && checker.get(minValue) > 0) {
+                    if (!result.contains(minValue)) {
+                        result.add(minValue);
+                    }
+                    min_pq.poll();
+                } else {
+                    min_pq.poll();
+                }
+            }
+            if (result.isEmpty()) {
+                System.out.println(EMPTY);
+            } else {
+                System.out.println(result.stream().max(Integer::compareTo).get() + " " + result.stream().min(Integer::compareTo).get());
+            }
         }
     }
 
-    private static void deleteValue(PriorityQueue<Integer> max_pq,
-                                    PriorityQueue<Integer> min_pq,
-                                    int flag) {
+
+    // TODO: 여기서, 2개의 우선순위 큐의 데이터 정합성이 안맞는 문제가 있음. 이걸 해결해야 할 듯.
+    private static Integer deleteValue(PriorityQueue<Integer> max_pq,
+                                       PriorityQueue<Integer> min_pq,
+                                       int flag) {
 
         if (flag == MAX_DEL) {
-            max_pq.poll();
-        } else if (flag == MIN_DEL) {
-            min_pq.poll();
+            return max_pq.poll();
+        } else {
+            return min_pq.poll();
         }
     }
 }
