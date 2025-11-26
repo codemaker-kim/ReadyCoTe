3273612fafe394107d6a7cc79b21e97c98ef02ec
codemaker-kim <tnals72441@daum.net>
2025-11-26 18:15:39 +0900

    1043번 중간 커밋

---
+++ src/main/java/baekjoon/b1043.java
@@ -0,0 +1,74 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class b1043 {
+
+    private static boolean[] visited;
+    private static Queue<Integer> knowTruthPeoples;
+    private static int[][] matrix;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // 사람의 수
+        int N = Integer.parseInt(sc.next());
+
+        // 파티의 수
+        int M = Integer.parseInt(sc.next());
+
+        // 진실을 아는 사람의 수
+        int knowTruthCount = Integer.parseInt(sc.next());
+
+        knowTruthPeoples = new LinkedList<>();
+        visited = new boolean[N + 1];
+        matrix = new int[M + 1][N + 1];
+
+        // 해봐야 50번
+        for (int index = 0; index < knowTruthCount; index++) {
+            int peopleNumber = Integer.parseInt(sc.next());
+
+            knowTruthPeoples.add(peopleNumber);
+            visited[peopleNumber] = true;
+
+        }
+
+        // 50 * 50 -> 2500번 정도
+        for (int partyNum = 0; partyNum < M; partyNum++) {
+            int partyPeopleCount = Integer.parseInt(sc.next());
+
+            List<Integer> partyPeoples = new ArrayList<>();
+
+            for (int peopleIndex = 0; peopleIndex < partyPeopleCount; peopleIndex++) {
+                int people = Integer.parseInt(sc.next());
+
+                partyPeoples.add(people);
+            }
+
+            if (partyPeoples.stream().anyMatch(partyPeoples::contains)) {
+                partyPeoples
+                        .forEach(people -> {
+                            if (!knowTruthPeoples.contains(people)) {
+                                knowTruthPeoples.add(people);
+                                visited[people] = true;
+                            }
+                        });
+            }
+        }
+
+
+
+        printResult();
+
+        sc.close();
+    }
+
+    private static void printResult() {
+
+    }
+}
