4a18a5592fad9c5abdf5c2545fd2679763be2a5c
codemaker-kim <tnals72441@daum.net>
2026-05-08 16:29:25 +0900

    프로그래머스 피로도 문제 해결

---
+++ src/main/java/programers/dbfs/bruteforce/피로도.java
@@ -0,0 +1,33 @@
+package programers.dbfs.bruteforce;
+
+class solution {
+
+    private static boolean[] visited;
+    private static int maxCount = 0;
+
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        calculateMaxExploreCount(k, dungeons, 0);
+
+        return maxCount;
+    }
+
+    public void calculateMaxExploreCount(int k, int[][] dungeons, int count) {
+        maxCount = Math.max(maxCount, count);
+
+        for (int index = 0; index < dungeons.length; index++) {
+            int[] dungeon = dungeons[index];
+
+            if (visited[index] || dungeon[0] > k) {
+                continue;
+            }
+
+            // DO & RECURSE
+            visited[index] = true;
+            calculateMaxExploreCount(k - dungeon[1], dungeons, count + 1);
+
+            // BACKTRACK
+            visited[index] = false;
+        }
+    }
+}
