5d45864f9547e2b627d8a1f59d7f5ffed5933fb6
codemaker-kim <tnals72441@daum.net>
2026-05-29 15:06:35 +0900

    sw 아카데미 덧셈과 뺄셈 해결

---
+++ src/main/java/sw_expert/problems/d3/P26792.java
@@ -0,0 +1,23 @@
+package sw_expert.problems.d3;
+
+import java.util.Scanner;
+
+public class P26792 {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int plus = sc.nextInt();
+            int minus = sc.nextInt();
+
+            int a = (plus + minus) / 2;
+            int b = plus - a;
+
+            System.out.println(a + " " + b);
+        }
+
+        sc.close();
+    }
+}
