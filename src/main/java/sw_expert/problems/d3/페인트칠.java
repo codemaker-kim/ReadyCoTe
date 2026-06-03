b4557c432fa53816c285d8781fec4e609cedbce2
codemaker-kim <tnals72441@daum.net>
2026-06-03 23:45:53 +0900

    sw 아카데미 페인트칠 문제 해결

--- src/main/java/sw_expert/problems/d3/페인트칠.java
+++ src/main/java/sw_expert/problems/d3/페인트칠.java
@@ -1,15 +1,8 @@
 package sw_expert.problems.d3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class 페인트칠 {
-
-    private static final String BLACK = "#";
-    private static final String WHITE = ".";
-
-    private static String[][] matrix;
-    private static boolean[] checked;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -19,48 +12,62 @@ public class 페인트칠 {
         for (int test_case = 1; test_case <= T; test_case++) {
             int H = sc.nextInt();
             int W = sc.nextInt();
-            sc.nextLine();
+            sc.nextLine(); // 정수 입력 후 버퍼에 남은 개행 문자(\n) 제거
 
-            matrix = new String[H][W];
-            checked = new boolean[W];
-            int answer = 0;
+            char[][] grid = new char[H][W];
+            boolean hasWhite = false;
 
-            // 매트릭스 초기화
+            // 격자판 초기화 및 흰색 존재 여부 체크
             for (int i = 0; i < H; i++) {
-                char[] c = sc.nextLine().toCharArray();
-
+                String line = sc.nextLine();
                 for (int j = 0; j < W; j++) {
-                    matrix[i][j] = Character.toString(c[j]);
-                }
-            }
-
-            // 격자 행, 혹은 열을 골라서 모두 검은색으로 칠하면,
-            // 여전히 검은색.
-            // 특정한 행에, 흰 색이 존재한다면, 그 행은 행 색칠을 하지 않았음.
-            // 1. 행에 검은 색이 보이는 순간, flag를 true로 돌린다.
-            // 2. flag가 true 라면 행에 있는 검은 색의 수를 센다.
-            // 3. 그렇지 않다면, 넘어간다.
-
-            for (int y = 0; y < matrix.length; y++) {
-                // 흰 색을 포함하지 않는다면, 행 색칠 연산을 수행했다.
-                if (!containsWhite(matrix[y]) && W >= H) {
-                    answer++;
-                    continue;
-                }
-
-                for (int x = 0; x < matrix[y].length; x++) {
-                    if (matrix[y][x].equals(BLACK) && !checked[x]) {
-                        answer++;
-                        checked[x] = true;
+                    grid[i][j] = line.charAt(j);
+                    if (grid[i][j] == '.') {
+                        hasWhite = true;
                     }
                 }
             }
 
-            System.out.println(answer);
-        }
-    }
+            // [예외 처리] 격자판에 흰색이 단 하나도 없는 경우
+            if (!hasWhite) {
+                System.out.println(Math.min(H, W));
+                continue;
+            }
 
-    private static boolean containsWhite(String[] row) {
-        return Arrays.asList(row).contains(WHITE);
+            // 1. 모든 칸이 '#'인 행(가로줄)의 개수 카운트
+            int allBlackRows = 0;
+            for (int i = 0; i < H; i++) {
+                boolean isAllBlack = true;
+                for (int j = 0; j < W; j++) {
+                    if (grid[i][j] == '.') {
+                        isAllBlack = false;
+                        break;
+                    }
+                }
+                if (isAllBlack) {
+                    allBlackRows++;
+                }
+            }
+
+            // 2. 모든 칸이 '#'인 열(세로줄)의 개수 카운트
+            int allBlackCols = 0;
+            for (int j = 0; j < W; j++) {
+                boolean isAllBlack = true;
+                for (int i = 0; i < H; i++) {
+                    if (grid[i][j] == '.') {
+                        isAllBlack = false;
+                        break;
+                    }
+                }
+                if (isAllBlack) {
+                    allBlackCols++;
+                }
+            }
+
+            // 최소 연산 횟수 출력
+            System.out.println(allBlackRows + allBlackCols);
+        }
+
+        sc.close();
     }
 }
